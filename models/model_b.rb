101d17b0e746d5dc8b3c9e672d5b3c0819ae3a6b
Philip Medlin <phmedlin@gmail.com>
2021-07-30 12:53:36 -0600

    Add yazoo_drive_house hash to model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+yazoo_drive_house = {
+  "address" => '1614 Yazoo Dr.',
+  "school_district" => 'Marion County',
+  "assessed_value_usd" => 295000,
+  "lot_size_in_sq_ft" => 10000,
+  "is_hud_loan_eligible" => false,
+  "is_in_flood_plain" => true,
+  "permits_on_file" => ['solar panel install', 'electrical panel replace', 'lead paint remediation'],
+  "utility_jurisdictions" => ['Xcel Energy', 'Central Valley Water Authority', 'Allied Waste Management']
+}
