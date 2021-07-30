cb2e9060e25ef127e1b03d3c02c1b7eb69c2114b
Philip Medlin <phmedlin@gmail.com>
2021-07-30 12:22:50 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+anmial = "Wolf" #Phil's note: The challenge says to call it "anmial" [sic]
+is_cuddly = false
+anmial = "Hippo"
+num_8 = 8.0
+num_3 = 3.0
+quotient = num_3 / num_8
+favorite_films = ["Titanic", "True Lies", "Dr. Strangelove"]
+favorite_films.delete_at(0)
+p anmial.size
