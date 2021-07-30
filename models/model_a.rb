5109666254b19c6c7b6d9825b53ce84e5bb6cb79
Philip Medlin <phmedlin@gmail.com>
2021-07-30 12:37:34 -0600

    Add macbook_pro hash to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+macbook_pro = {
+  "owner" => 'Phil M.',
+  "condtion" => 'like new',
+  "number_of_owners" => 1,
+  "value_in_usd" => 1200,
+  "includes_accessories" => true,
+  "includes_original_packaging" => false,
+  "search_descriptors" => ['macbook pro', 'used laptop', 'mac laptop'],
+  "applications_installed" => ['atom', 'rectangle', 'zoom']
+}
